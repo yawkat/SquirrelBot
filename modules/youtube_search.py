94623a4d0b0ff75e4d8ee2a6548806f178c627df
Jonas Konrad <me@yawk.at>
2014-07-31 14:19:29 +0200

    Fix some spam

--- modules/youtube_search.py
+++ modules/youtube_search.py
@@ -20,13 +20,13 @@ class YoutubeSearch(StatusModule):
     def _poll(self):
         url = "https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&q=%s&key=%s" % (urllib.quote(self.search_term, ""), self.api_key)
         if self.last_upload_time is not None:
-            url += "&publishedAfter=" + self.last_upload_time
+            url += "&publishedAfter=" + urllib.quote(self.last_upload_time, "")
         req = urllib2.urlopen(url)
         response = json.load(req)
         if self.last_upload_time is not None:
             for video in response["items"][:3]: # limit to 3 videos
                 if video["snippet"]["publishedAt"] == self.last_upload_time:
-                    continue
+                    break
                 message = BOLD + "[youtube]" + RESET + " \"" + video["snippet"]["title"] + "\" by " + video["snippet"]["channelTitle"]
                 self._broadcast(message)
         if len(response["items"]) is not 0:

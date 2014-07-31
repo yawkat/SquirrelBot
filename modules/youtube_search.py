761b879cb7c50ed981e964fd3032b131150aaf76
Jonas Konrad <me@yawk.at>
2014-07-31 14:01:26 +0200

    I can actually shorten that

--- modules/youtube_search.py
+++ modules/youtube_search.py
@@ -8,25 +8,14 @@ import time
 import traceback
 import threading
 
-class YoutubeSearch(Module):
+class YoutubeSearch(StatusModule):
     def __init__(self, id, api_key, channels, search_term):
-        super(YoutubeSearch, self).__init__(id)
+        super(YoutubeSearch, self).__init__(id, on=channels, rate=30)
         self.api_key = api_key
         self.channels = channels
         self.search_term = search_term
 
         self.last_upload_time = None
-
-    def init(self):
-        threading.Thread(target=self._repeated_poll).start()
-
-    def _repeated_poll(self):
-        while 1:
-            try:
-                self._poll()
-            except:
-                traceback.print_exc()
-            time.sleep(20)
 
     def _poll(self):
         url = "https://www.googleapis.com/youtube/v3/search?part=snippet&order=date&q=%s&key=%s" % (urllib.quote(self.search_term, ""), self.api_key)
@@ -42,9 +31,4 @@ class YoutubeSearch(Module):
                 self._broadcast(message)
         if len(response["items"]) is not 0:
             self.last_upload_time = response["items"][0]["snippet"]["publishedAt"]
-
-    def _broadcast(self, message):
-        for name in self.on:
-            if name in self.channels:
-                self.channels[name].chat(message)
                 
